c1409b5921c01ce7a56f16c8509f5d5626a139ae
geusan <dnay2k@gmail.com>
2024-12-01 01:04:24 +0900

    fix: unused library

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import envCompatiblePlugin from 'vite-plugin-env-compatible';
 import path from 'path';
-import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +10,6 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     envCompatiblePlugin(),
-    svgr({
-      svgrOptions: {
-        icon: true, // SVG를 ReactComponent로 사용할 수 있도록 설정
-      },
-    }),
   ],
   define: {
     'process.env': process.env,
