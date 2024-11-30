3d90347123a43aa16d94152be5cbeb201c2aa14d
yunah <favoryun12@gmail.com>
2024-11-30 19:32:38 +0900

    Feat.Detail Reservation Button 추가

---
+++ src/components/details/DetailReservationBtn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailReservationBtn = () => {
+  return (
+    <div className="fixed bottom-0 left-0 w-full p-4 flex justify-center">
+      <button className="w-96 max-w-md bg-main text-white py-4 rounded-3xl shadow-lg">예약하기</button>
+    </div>
+  );
+};
+
+export default DetailReservationBtn;
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,6 +1,7 @@
 import DetailAccessInfo from '@/components/details/DetailAccessInfo';
 import DetailBasicInfo from '@/components/details/DetailbasicInfo';
 import DetailIntroInfo from '@/components/details/DetailIntroInfo';
+import DetailReservationBtn from '@/components/details/DetailReservationBtn';
 
 const DetailPage = () => {
   return (
@@ -8,6 +9,7 @@ const DetailPage = () => {
       <DetailIntroInfo />
       <DetailAccessInfo />
       <DetailBasicInfo />
+      <DetailReservationBtn />
     </div>
   );
 };
